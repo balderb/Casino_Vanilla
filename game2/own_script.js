af348986ab186c8b1af02e64a2a21bb4c611016c
Balder Bongaerts <59473814+balderb@users.noreply.github.com>
2020-07-30 15:26:04 +0200

    starting from the beginning

---
+++ game2/own_script.js
@@ -0,0 +1,7 @@
+const suits = ["spades", "diamonds", "clubs", "hearts"]; // The variable suits is an array of "card suites" (kind of of category) that we will use as a lookup table
+const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]; // this value will hold an array with all the possible values a card can have
+
+function createDeck() { // we create a function createdeck (back-end function)
+    deck = new Array();
+}
+
