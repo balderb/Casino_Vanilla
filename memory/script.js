3cf3776b29689d7c6a32c128b6d7f61aef0d728e
Lisa Baetsle <lisa_baetsle@hotmail.com>
2020-08-03 11:12:40 +0200

    add script: random array

--- memory/script.js
+++ memory/script.js
@@ -1,5 +1,5 @@
 // Make an array for the random order of the cards
-
+let cards = [1 ,1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6];
 
 
 // Add an image to the array
@@ -7,17 +7,17 @@
 
 
 // Make the function to randomize the order of the cards
-
+const rdmCards = Math.floor(Math.random() * cards.length);
+console.log(cards[rdmCards]);
 
 
 
 // Create the start game button, and when clicked, change to restart game
 
 
-
-
 // Click on the first card --> turns around, remembers value
+
 
 // Click on the second card --> turns around, remembers value and compare the values
 // if cards are the same, get a point and leave them turned
-// if cards are different, turn them around again and start from the beginning
+// if cards are different, turn them around again and start from where you left off
