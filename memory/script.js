438304b09f9a9e7a54fa460baddfa1e172dab94d
Lisa Baetsle <lisa_baetsle@hotmail.com>
2020-08-03 10:11:00 +0200

    Logic of game 2: memory

---
+++ memory/script.js
@@ -0,0 +1,23 @@
+// Make an array for the random order of the cards
+
+
+
+// Add an image to the array
+
+
+
+// Make the function to randomize the order of the cards
+
+
+
+
+// Create the start game button, and when clicked, change to restart game
+
+
+
+
+// Click on the first card --> turns around, remembers value
+
+// Click on the second card --> turns around, remembers value and compare the values
+// if cards are the same, get a point and leave them turned
+// if cards are different, turn them around again and start from the beginning
