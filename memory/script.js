5078180d4e5b2a48ed82f397533b8d10182b9ab9
Lisa Baetsle <lisa_baetsle@hotmail.com>
2020-08-04 10:51:53 +0200

    add html structure and basic styles

--- memory/script.js
+++ memory/script.js
@@ -1,23 +1,7 @@
-// Make an array for the random order of the cards
-let cards = [1 ,1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6];
+const cards = document.querySelectorAll('.memory-card');
 
+function flipCard() {
+  this.classList.toggle('flip');
+}
 
-// Add an image to the array
-
-
-
-// Make the function to randomize the order of the cards
-const rdmCards = Math.floor(Math.random() * cards.length);
-console.log(cards[rdmCards]);
-
-
-
-// Create the start game button, and when clicked, change to restart game
-
-
-// Click on the first card --> turns around, remembers value
-
-
-// Click on the second card --> turns around, remembers value and compare the values
-// if cards are the same, get a point and leave them turned
-// if cards are different, turn them around again and start from where you left off
+cards.forEach(card => card.addEventListener('click', flipCard));
